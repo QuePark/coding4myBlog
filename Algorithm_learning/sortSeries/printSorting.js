0e417965a5f501fda2075b61bbee78358a5ee743
QuePark <gyuhapark11@gmail.com>
2020-08-30 14:50:24 +0900

    Create quick sort function

--- Algorithm_learning/sortSeries/printSorting.js
+++ Algorithm_learning/sortSeries/printSorting.js
@@ -1,20 +1,26 @@
 import { bubbleSort } from './bubbleSort.js';
 import { insertionSort } from './insertionSort.js';
+import { selectionSort } from './selectionSort.js';
+import { mergeSort } from './mergeSort.js';
+import { quickSort } from './quickSort.js';
 
-export const print = (ranArr, sortingFn) => {
-	for (let i = 0; i < ranArr.length; i++) {
-		let str = '';
-		for (let j = 0; j < ranArr[i]; j++) {
-			str += '*';
+export const print = (ranArr, sortingFn, flag = true) => {
+	if (flag) {
+		for (let i = 0; i < ranArr.length; i++) {
+			let str = '';
+			for (let j = 0; j < ranArr[i]; j++) {
+				str += '*';
+			}
+			console.log(str, ranArr[i]);
 		}
-		console.log(str, ranArr[i]);
+
+		console.log('==========================');
+		console.log('==========================');
+		console.log('==========================');
 	}
 
-	console.log('==========================');
-	console.log('==========================');
-	console.log('==========================');
 	if (typeof sortingFn === 'function') {
-		sortingFn(ranArr, true);
+		return sortingFn(ranArr, flag);
 	}
 };
 
@@ -28,7 +34,28 @@ const generateRandomNumberArr = (max) => {
 
 const init = () => {
 	const randomNumberArray = generateRandomNumberArr(10);
-	print(randomNumberArray, insertionSort);
+	let test = [...randomNumberArray];
+	const flag = false;
+	let result;
+	console.log('start: ', randomNumberArray);
+	result = print(test, bubbleSort, false);
+	console.log('bubbleSort: ', result);
+	test = [...randomNumberArray];
+	flag ? console.log('start: ', test) : console.log('');
+	result = print(test, insertionSort, false);
+	console.log('insertionSort: ', result);
+	test = [...randomNumberArray];
+	flag ? console.log('start: ', test) : console.log('');
+	result = print(test, selectionSort, false);
+	console.log('selectionSort: ', result);
+	test = [...randomNumberArray];
+	flag ? console.log('start: ', test) : console.log('');
+	result = print(test, mergeSort, false);
+	console.log('mergeSort: ', result);
+	test = [...randomNumberArray];
+	flag ? console.log('start: ', test) : console.log('');
+	result = print(test, quickSort, false);
+	console.log('quickSort: ', result);
 };
 
 init();
